28874084e9999ec8834d97911ecf60daec7ea5f1
Axel Raboit <axel.raboit@gmail.com>
2022-03-04 09:25:50 +0100

    add some url modification into the routes "/api/"

--- routes/routes.js
+++ routes/routes.js
@@ -14,27 +14,36 @@ import {
 const router = express.Router();
 
 //Route Racine
-router.get('/', getRoot)
+router.get('/api/', getRoot)
+
 
 //Route Test pour récuperer des informations
-router.get('/test', getTest)
+router.get('/api/test', (req,res) => {
+    res.send({
+        name: 'Hello World from json data'
+    })
+})
+
+
+//Route Test pour récuperer des informations
+router.get('/api/test', getTest)
 
 //Route Test pour poster des informations
-router.post('/test', postTest)
+router.post('/api/test', postTest)
 
 //Route pour créer une room
-router.post('/room', addRoom)
+router.post('/api/rooms', addRoom)
 
 //Route pour récuperer les rooms
-router.get('/rooms', getRooms)
+router.get('/api/rooms', getRooms)
 
 //Route pour récuperer une chambre par son id
-router.get('/room/:id', getRoom)
+router.get('/api/rooms/:id', getRoom)
 
 //Route pour update une chambre par son id
-router.patch('/room/:id', updateRoom)
+router.patch('/api/rooms/:id', updateRoom)
 
 //Route pour delete une chambre par son id
-router.delete('/room/:id', deleteRoom)
+router.delete('/api/rooms/:id', deleteRoom)
 
 export default router;
